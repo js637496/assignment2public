f0008b6def6dcb3d7e469d1b0b79ec9d17c82621
Jim Speers <jimspeers.speers@gmail.com>
2021-08-02 16:38:24 -0400

    Update webserver_setup.sh

--- webserver_setup.sh
+++ webserver_setup.sh
@@ -11,5 +11,5 @@ sudo apt -y install nfs-common
 #Can not mount until nfs server is up and running
 sudo mkdir -p /var/webserver_log
 sudo chown -R nobody:nogroup /var/webserver_log
-sudo chmod 755 /var/webserver_log/
+sudo chmod 777 /var/webserver_log/
 sudo chmod 755 /local/repository/scan.sh
