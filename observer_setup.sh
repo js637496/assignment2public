0ba31263058b93bc85db1197c210c88cda9805f7
Jim Speers <jimspeers.speers@gmail.com>
2021-08-02 12:19:27 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -3,7 +3,8 @@
 sudo apt -y install nfs-kernel-server
 sudo mkdir -p /var/webserver_monitor
 sudo chown -R nobody:nogroup /var/webserver_monitor
-sudo chmod 755 /var/webserver_monitor/
+sudo chmod 777 /var/webserver_monitor/
+
 
 sudo echo '/var/webserver_monitor 192.168.1.0/24(rw,sync,no_subtree_check)' | sudo tee -a /etc/exports
 sudo exportfs -a
