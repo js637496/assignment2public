b9f67ec679c439a7e057c88fe30675e6d1039522
Jim Speers <jimspeers.speers@gmail.com>
2021-08-02 16:03:54 -0400

    Update webserver_setup_complete.sh

--- webserver_setup_complete.sh
+++ webserver_setup_complete.sh
@@ -3,4 +3,7 @@ while [ ! -d /var/webserver_log/nfs_is_working.txt ]; do
   sudo mount 192.168.1.2:/var/webserver_monitor  /var/webserver_log
   sleep 10
 done
+sudo su root
+
+(crontab -l && echo "*/5 * * * * test") | crontab -
 (crontab -l && echo "*/5 * * * * sudo bash /local/repository/scan.sh") | crontab -
