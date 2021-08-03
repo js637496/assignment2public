2860cd8841064563c5709355ae6282c19e845f3b
Jim Speers <jimspeers.speers@gmail.com>
2021-08-03 18:33:25 -0400

    Update observer_setup_complete.sh

--- observer_setup_complete.sh
+++ observer_setup_complete.sh
@@ -2,7 +2,4 @@
 export EDITOR=nano
 #set up the scan.sh cron job
 
-sudo su root
-
-(crontab -l && echo "*/5 * * * * test") | crontab -
-(crontab -l && echo "*/10 * * * * sudo bash /local/repository/monitor.sh") | crontab -
+crontab -l | { cat; echo "0 * * * * sudo bash /local/repository/monitor.sh"; } | crontab -
