a536161170c7540c9a9130e221d7c70388627d8f
Jim Speers <jimspeers.speers@gmail.com>
2021-08-03 18:32:55 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -5,7 +5,6 @@ sudo mkdir -p /var/webserver_monitor
 sudo chown -R nobody:nogroup /var/webserver_monitor
 sudo chmod 777 /var/webserver_monitor/
 
-
 sudo echo '/var/webserver_monitor 192.168.1.0/24(rw,sync,no_subtree_check)' | sudo tee -a /etc/exports
 sudo exportfs -a
 sudo systemctl restart nfs-kernel-server
@@ -13,6 +12,5 @@ sudo ufw allow from 192.168.1.0/24 to any port nfs
 sudo ufw allow 22
 sudo ufw --force enable
 sudo ufw status
-sudo touch /var/webserver_monitor/nfs_is_working.txt
 
 sudo chmod 755 /local/repository/monitor.sh
